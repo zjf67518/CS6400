eb45d960ebd0f778d9b687ebd0d0327090f0572c
huahua <1295466052@qq.com>
2021-11-06 14:54:09 +0800

    搜索车辆信息

--- src/main/java/com/cs6400/carshop/utils/converter/SearchInfoConverter.java
+++ src/main/java/com/cs6400/carshop/utils/converter/SearchInfoConverter.java
@@ -1,5 +1,6 @@
 package com.cs6400.carshop.utils.converter;
 
+
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,9 +8,13 @@ import java.math.BigDecimal;
 @Data
 public class SearchInfoConverter {
     private String VIN;
-    private Integer vehcle_type;
+    private Integer vehicle_type;
     private Integer model_year;
     private String manufacturer_name;
     private String color;
     private BigDecimal list_price;
+    private Boolean moreThanPrice;
+    private String keyword;
+    private String order;
+    private Boolean descOrder;
 }
