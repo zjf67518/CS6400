7b6b6509974348e168ec1907f6467643a845573b
huahua <1295466052@qq.com>
2021-11-21 21:14:19 +0800

    查询顾客信息

--- src/main/java/com/cs6400/carshop/mapper/CustomerMapper.java
+++ src/main/java/com/cs6400/carshop/mapper/CustomerMapper.java
@@ -1,5 +1,11 @@
 package com.cs6400.carshop.mapper;
 
-public interface CustomerMapper {
+import com.cs6400.carshop.bean.Customer;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface CustomerMapper {
+    Customer selectCustomerById(int id);
+    int insertIndividualCustomer(Customer customer);
+    int insertBusinessCustomer(Customer customer);
 }
--- src/main/java/com/cs6400/carshop/mapper/TruckMapper.java
+++ src/main/java/com/cs6400/carshop/mapper/TruckMapper.java
@@ -1,4 +1,8 @@
 package com.cs6400.carshop.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+
 public interface TruckMapper {
+
 }
