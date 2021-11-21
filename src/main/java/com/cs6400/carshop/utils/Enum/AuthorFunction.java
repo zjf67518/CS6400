fef1081931a0285480c8ef400f5a3976d12a225d
kevenzhu <1091639781@qq.com>
2021-11-21 16:09:06 +0800

    vehicle_type to vehicle_name

--- src/main/java/com/cs6400/carshop/utils/Enum/AuthorFunction.java
+++ src/main/java/com/cs6400/carshop/utils/Enum/AuthorFunction.java
@@ -1,5 +1,12 @@
 package com.cs6400.carshop.utils.Enum;
 
 public enum AuthorFunction {
+    AddCustomer(1), findCustomer(2), AddVehicle(4), SellVehicle(8),
+    RepairVehicle(16), VehicleNumber(32), VehicleDetail(64), PirceFree(128);
 
+    private int type;
+
+    private AuthorFunction(int type) {
+
+    }
 }
---
+++ src/main/java/com/cs6400/carshop/utils/Enum/PrivilegedUser.java
@@ -0,0 +1,12 @@
+package com.cs6400.carshop.utils.Enum;
+
+public enum PrivilegedUser {
+    inventory_clerk(6), sales_person(11), service_writer(19), manager(34), owner(255);
+
+    private PrivilegedUser(int type) {
+
+    }
+
+
+
+}
