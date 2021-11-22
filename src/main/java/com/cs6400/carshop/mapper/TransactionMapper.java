555470bbc0ed6975bda570f5d2f12593caa3dae8
huahua <1295466052@qq.com>
2021-11-22 11:58:50 +0800

    添加顾客信息

--- src/main/java/com/cs6400/carshop/mapper/TransactionMapper.java
+++ src/main/java/com/cs6400/carshop/mapper/TransactionMapper.java
@@ -1,4 +1,9 @@
 package com.cs6400.carshop.mapper;
 
+import com.cs6400.carshop.bean.Transaction;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface TransactionMapper {
+    int insertTransaction(Transaction transaction);
 }
